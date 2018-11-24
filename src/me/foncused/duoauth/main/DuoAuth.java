2d57fd38910a4237e684a932e1e50e87fec4102a
foncused <drewerd615@hotmail.com>
2018-11-24 11:32:08 -0500

    First commit
    First commit

---
+++ src/me/foncused/duoauth/main/DuoAuth.java
@@ -0,0 +1,17 @@
+package me.foncused.duoauth.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DuoAuth extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+}
