ac951f6f44f8fc8655eddc5867f05c7c1851bfa4
foncused <foncused@users.noreply.github.com>
2018-12-20 22:15:38 -0500

    Match case
    Match case

--- src/me/foncused/duoauth/enumerable/DuoAuthMessage.java
+++ src/me/foncused/duoauth/enumerable/DuoAuthMessage.java
@@ -4,7 +4,7 @@ import org.bukkit.ChatColor;
 
 public enum DuoAuthMessage {
 
-	PLAYER_NOT_AUTHED(ChatColor.RED + "You need to authenticate with /auth <password> <PIN> before you can chat, move, or play."),
+	PLAYER_NOT_AUTHED(ChatColor.RED + "You need to authenticate with /auth <password> <pin> before you can chat, move, or play."),
 	PLAYER_NOT_DATABASED(ChatColor.RED + "You have not set up authentication. To enable authentication, please use the /auth command."),
 	MUST_WAIT(ChatColor.RED + "You must wait before doing that again."),
 	NO_PERMISSION(ChatColor.RED + "You do not have permission to do this!"),
