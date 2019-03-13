411a4e898ee9e5b5e7241709209fc5a2c054c28d
foncused <foncused@users.noreply.github.com>
2019-03-12 23:24:57 -0400

    Ordering

--- src/me/foncused/duoauth/config/LangManager.java
+++ src/me/foncused/duoauth/config/LangManager.java
@@ -2,116 +2,96 @@ package me.foncused.duoauth.config;
 
 public class LangManager {
 
-	private final String prefixAlert;
-	private final String prefixNotify;
-	private final String playerNotAuthed;
-	private final String playerNotDb;
-	private final String noConsole;
-	private final String mustWait;
-	private final String noPermission;
-	private final String locked;
 	private final String authInProgress;
 	private final String authInProgressAdmin;
-	private final String sessionExpired;
-	private final String deauthSuccess;
-	private final String deauthFailed;
-	private final String deauthAdminSuccess;
-	private final String resetSuccess;
-	private final String resetFailed;
-	private final String resetAdminSuccess;
-	private final String settingUp;
-	private final String settingUpSuccess;
-	private final String settingUpFailed;
 	private final String authenticating;
-	private final String authenticatingSuccess;
 	private final String authenticatingFailed;
+	private final String authenticatingSuccess;
+	private final String deauthAdminSuccess;
+	private final String deauthFailed;
+	private final String deauthSuccess;
 	private final String enforced;
 	private final String loading;
+	private final String locked;
+	private final String mustWait;
+	private final String noConsole;
+	private final String noPermission;
+	private final String playerNotAuthed;
+	private final String playerNotDb;
+	private final String prefixAlert;
+	private final String prefixNotify;
+	private final String resetAdminSuccess;
+	private final String resetFailed;
+	private final String resetSuccess;
+	private final String sessionExpired;
+	private final String settingUp;
+	private final String settingUpFailed;
+	private final String settingUpSuccess;
 
 	public LangManager(
-		final String prefixAlert,
-		final String prefixNotify,
-		final String playerNotAuthed,
-		final String playerNotDb,
-		final String noConsole,
-		final String mustWait,
-		final String noPermission,
-		final String locked,
 		final String authInProgress,
 		final String authInProgressAdmin,
-		final String sessionExpired,
-		final String deauthSuccess,
-		final String deauthFailed,
-		final String deauthAdminSuccess,
-		final String resetSuccess,
-		final String resetFailed,
-		final String resetAdminSuccess,
-		final String settingUp,
-		final String settingUpSuccess,
-		final String settingUpFailed,
 		final String authenticating,
-		final String authenticatingSuccess,
 		final String authenticatingFailed,
+		final String authenticatingSuccess,
+		final String deauthAdminSuccess,
+		final String deauthFailed,
+		final String deauthSuccess,
 		final String enforced,
-		final String loading
+		final String loading,
+		final String locked,
+		final String mustWait,
+		final String noConsole,
+		final String noPermission,
+		final String playerNotAuthed,
+		final String playerNotDb,
+		final String prefixAlert,
+		final String prefixNotify,
+		final String resetAdminSuccess,
+		final String resetFailed,
+		final String resetSuccess,
+		final String sessionExpired,
+		final String settingUp,
+		final String settingUpFailed,
+		final String settingUpSuccess
 	) {
-		this.prefixAlert = prefixAlert;
-		this.prefixNotify = prefixNotify;
-		this.playerNotAuthed = playerNotAuthed;
-		this.playerNotDb = playerNotDb;
-		this.noConsole = noConsole;
-		this.mustWait = mustWait;
-		this.noPermission = noPermission;
-		this.locked = locked;
 		this.authInProgress = authInProgress;
 		this.authInProgressAdmin = authInProgressAdmin;
-		this.sessionExpired = sessionExpired;
-		this.deauthSuccess = deauthSuccess;
-		this.deauthFailed = deauthFailed;
-		this.deauthAdminSuccess = deauthAdminSuccess;
-		this.resetSuccess = resetSuccess;
-		this.resetFailed = resetFailed;
-		this.resetAdminSuccess = resetAdminSuccess;
-		this.settingUp = settingUp;
-		this.settingUpSuccess = settingUpSuccess;
-		this.settingUpFailed = settingUpFailed;
 		this.authenticating = authenticating;
-		this.authenticatingSuccess = authenticatingSuccess;
 		this.authenticatingFailed = authenticatingFailed;
+		this.authenticatingSuccess = authenticatingSuccess;
+		this.deauthAdminSuccess = deauthAdminSuccess;
+		this.deauthFailed = deauthFailed;
+		this.deauthSuccess = deauthSuccess;
 		this.enforced = enforced;
 		this.loading = loading;
+		this.locked = locked;
+		this.mustWait = mustWait;
+		this.noConsole = noConsole;
+		this.noPermission = noPermission;
+		this.playerNotAuthed = playerNotAuthed;
+		this.playerNotDb = playerNotDb;
+		this.prefixAlert = prefixAlert;
+		this.prefixNotify = prefixNotify;
+		this.resetAdminSuccess = resetAdminSuccess;
+		this.resetFailed = resetFailed;
+		this.resetSuccess = resetSuccess;
+		this.sessionExpired = sessionExpired;
+		this.settingUp = settingUp;
+		this.settingUpFailed = settingUpFailed;
+		this.settingUpSuccess = settingUpSuccess;
 	}
 
-	public synchronized String getPrefixAlert() {
-		return this.prefixAlert;
+	public synchronized String getAuthenticating() {
+		return this.authenticating;
 	}
 
-	public synchronized String getPrefixNotify() {
-		return this.prefixNotify;
+	public synchronized String getAuthenticatingFailed() {
+		return this.authenticatingFailed;
 	}
 
-	public synchronized String getPlayerNotAuthed() {
-		return this.playerNotAuthed;
-	}
-
-	public synchronized String getPlayerNotDb() {
-		return this.playerNotDb;
-	}
-
-	public synchronized String getNoConsole() {
-		return this.noConsole;
-	}
-
-	public synchronized String getMustWait() {
-		return this.mustWait;
-	}
-
-	public synchronized String getNoPermission() {
-		return this.noPermission;
-	}
-
-	public synchronized String getLocked() {
-		return this.locked;
+	public synchronized String getAuthenticatingSuccess() {
+		return this.authenticatingSuccess;
 	}
 
 	public synchronized String getAuthInProgress() {
@@ -122,56 +102,16 @@ public class LangManager {
 		return this.authInProgressAdmin;
 	}
 
-	public synchronized String getSessionExpired() {
-		return this.sessionExpired;
-	}
-
-	public synchronized String getDeauthSuccess() {
-		return this.deauthSuccess;
+	public synchronized String getDeauthAdminSuccess() {
+		return this.deauthAdminSuccess;
 	}
 
 	public synchronized String getDeauthFailed() {
 		return this.deauthFailed;
 	}
 
-	public synchronized String getDeauthAdminSuccess() {
-		return this.deauthAdminSuccess;
-	}
-
-	public synchronized String getResetSuccess() {
-		return this.resetSuccess;
-	}
-
-	public synchronized String getResetFailed() {
-		return this.resetFailed;
-	}
-
-	public synchronized String getResetAdminSuccess() {
-		return this.resetAdminSuccess;
-	}
-
-	public synchronized String getSettingUp() {
-		return this.settingUp;
-	}
-
-	public synchronized String getSettingUpSuccess() {
-		return this.settingUpSuccess;
-	}
-
-	public synchronized String getSettingUpFailed() {
-		return this.settingUpFailed;
-	}
-
-	public synchronized String getAuthenticating() {
-		return this.authenticating;
-	}
-
-	public synchronized String getAuthenticatingSuccess() {
-		return this.authenticatingSuccess;
-	}
-
-	public synchronized String getAuthenticatingFailed() {
-		return this.authenticatingFailed;
+	public synchronized String getDeauthSuccess() {
+		return this.deauthSuccess;
 	}
 
 	public synchronized String getEnforced() {
@@ -180,6 +120,66 @@ public class LangManager {
 
 	public synchronized String getLoading() {
 		return this.loading;
+	}
+
+	public synchronized String getLocked() {
+		return this.locked;
+	}
+
+	public synchronized String getMustWait() {
+		return this.mustWait;
+	}
+
+	public synchronized String getNoConsole() {
+		return this.noConsole;
+	}
+
+	public synchronized String getNoPermission() {
+		return this.noPermission;
+	}
+
+	public synchronized String getPlayerNotAuthed() {
+		return this.playerNotAuthed;
+	}
+
+	public synchronized String getPlayerNotDb() {
+		return this.playerNotDb;
+	}
+
+	public synchronized String getPrefixAlert() {
+		return this.prefixAlert;
+	}
+
+	public synchronized String getPrefixNotify() {
+		return this.prefixNotify;
+	}
+
+	public synchronized String getResetAdminSuccess() {
+		return this.resetAdminSuccess;
+	}
+
+	public synchronized String getResetFailed() {
+		return this.resetFailed;
+	}
+
+	public synchronized String getResetSuccess() {
+		return this.resetSuccess;
+	}
+
+	public synchronized String getSessionExpired() {
+		return this.sessionExpired;
+	}
+
+	public synchronized String getSettingUp() {
+		return this.settingUp;
+	}
+
+	public synchronized String getSettingUpFailed() {
+		return this.settingUpFailed;
+	}
+
+	public synchronized String getSettingUpSuccess() {
+		return this.settingUpSuccess;
 	}
 
 }
--- src/me/foncused/duoauth/enumerable/AuthMessage.java
+++ src/me/foncused/duoauth/enumerable/AuthMessage.java
@@ -2,32 +2,31 @@ package me.foncused.duoauth.enumerable;
 
 public enum AuthMessage {
 
-	PREFIX_ALERT("&3ALERT &8» &7"),
-	PREFIX_NOTIFY("&cALERT &8» &7&o"),
-
-	PLAYER_NOT_AUTHED("&cYou need to authenticate with /auth <password> <pin> before you can chat, move, or play."),
-	PLAYER_NOT_DB("&cYou have not set up authentication. To enable authentication, please use the /auth command."),
-	NO_CONSOLE("&cYou cannot do this from the console."),
-	MUST_WAIT("&cYou must wait before doing that again."),
-	NO_PERMISSION("&cYou do not have permission to do this!"),
-	LOCKED("&cYour account has been locked. You will need to wait for your account to be unlocked, or you may contact the server administrators for assistance."),
+	AUTHENTICATING("&6Authenticating..."),
+	AUTHENTICATING_FAILED("&cAuthentication failed. Please ensure your password and PIN are correct. Please contact the server administrators if you believe that this is in error."),
+	AUTHENTICATING_SUCCESS("&aAuthentication successful. Have fun!"),
 	AUTH_IN_PROGRESS("&7Authentication in progress - please be patient..."),
 	AUTH_IN_PROGRESS_ADMIN("&cThat user is currently performing an authentication attempt. Please try again in a moment."),
-	SESSION_EXPIRED("&cYour session has expired. Please use the /auth command to continue playing. Thank you!"),
-	DEAUTH_SUCCESS("&aYour have deauthenticated successfully. To continue playing, please use the &c/auth &acommand."),
-	DEAUTH_FAILED("&cDeauthentication failed. Please contact the server administrators if you are receiving this message."),
 	DEAUTH_ADMIN_SUCCESS("&cYou have been deauthenticated by an administrator. Please use the /auth command to continue playing. Thank you!"),
-	RESET_SUCCESS("&aYour credentials have been reset! To re-enable authentication, please use the &c/auth &acommand."),
-	RESET_FAILED("&cFailed to reset authentication. Please contact the server administrators if you are receiving this message."),
-	RESET_ADMIN_SUCCESS("&aYour credentials have been reset by an administrator."),
-	SETTING_UP("&6Setting up authentication..."),
-	SETTING_UP_SUCCESS("&aYour credentials have been set!"),
-	SETTING_UP_FAILED("&cFailed to set up authentication. Please contact the server administrators if you are receiving this message."),
-	AUTHENTICATING("&6Authenticating..."),
-	AUTHENTICATING_SUCCESS("&aAuthentication successful. Have fun!"),
-	AUTHENTICATING_FAILED("&cAuthentication failed. Please ensure your password and PIN are correct. Please contact the server administrators if you believe that this is in error."),
+	DEAUTH_FAILED("&cDeauthentication failed. Please contact the server administrators if you are receiving this message."),
+	DEAUTH_SUCCESS("&aYour have deauthenticated successfully. To continue playing, please use the &c/auth &acommand."),
 	ENFORCED("&cThe server administrator has required you to set up authentication. Please enter the command '/auth <password> <pin>' using the credentials given to you, and then use '/auth reset' to set your own credentials. Thank you!"),
-	LOADING("&cDuoAuth is still loading. Please try again in a moment.");
+	LOADING("&cDuoAuth is still loading. Please try again in a moment."),
+	LOCKED("&cYour account has been locked. You will need to wait for your account to be unlocked, or you may contact the server administrators for assistance."),
+	MUST_WAIT("&cYou must wait before doing that again."),
+	NO_CONSOLE("&cYou cannot do this from the console."),
+	NO_PERMISSION("&cYou do not have permission to do this!"),
+	PLAYER_NOT_AUTHED("&cYou need to authenticate with /auth <password> <pin> before you can chat, move, or play."),
+	PLAYER_NOT_DB("&cYou have not set up authentication. To enable authentication, please use the /auth command."),
+	PREFIX_ALERT("&3ALERT &8» &7"),
+	PREFIX_NOTIFY("&cALERT &8» &7&o"),
+	RESET_ADMIN_SUCCESS("&aYour credentials have been reset by an administrator."),
+	RESET_FAILED("&cFailed to reset authentication. Please contact the server administrators if you are receiving this message."),
+	RESET_SUCCESS("&aYour credentials have been reset! To re-enable authentication, please use the &c/auth &acommand."),
+	SESSION_EXPIRED("&cYour session has expired. Please use the /auth command to continue playing. Thank you!"),
+	SETTING_UP("&6Setting up authentication..."),
+	SETTING_UP_FAILED("&cFailed to set up authentication. Please contact the server administrators if you are receiving this message."),
+	SETTING_UP_SUCCESS("&aYour credentials have been set!");
 
 	private final String message;
 
