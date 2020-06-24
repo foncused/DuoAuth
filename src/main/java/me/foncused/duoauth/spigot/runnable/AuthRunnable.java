d22d9d1529c1be401536ff9f48aa093ebab821ca
foncused <foncused@users.noreply.github.com>
2020-06-24 10:31:34 -0400

    Update AuthRunnable.java

--- src/main/java/me/foncused/duoauth/spigot/runnable/AuthRunnable.java
+++ src/main/java/me/foncused/duoauth/spigot/runnable/AuthRunnable.java
@@ -67,7 +67,7 @@ public class AuthRunnable {
 					});
 				}
 			}
-		}.runTaskTimerAsynchronously(this.plugin, 0,  this.cm.getDeauthTimeoutCheckHeartbeat() * 60 * 20);
+		}.runTaskTimerAsynchronously(this.plugin, 0, this.cm.getDeauthTimeoutCheckHeartbeat() * 60 * 20);
 	}
 
 	public void runUnlockTask() {
