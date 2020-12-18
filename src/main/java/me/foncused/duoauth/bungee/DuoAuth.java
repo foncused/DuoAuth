e7b83e48cf1372e9e9917550222de494cba9f5d9
foncused <foncused@users.noreply.github.com>
2020-12-18 15:50:51 -0500

    Update DuoAuth.java

--- src/main/java/me/foncused/duoauth/bungee/DuoAuth.java
+++ src/main/java/me/foncused/duoauth/bungee/DuoAuth.java
@@ -1,18 +1,22 @@
 package me.foncused.duoauth.bungee;
 
 import me.foncused.duoauth.bungee.event.Event;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class DuoAuth extends Plugin {
 
+	private ProxyServer proxy;
+
 	@Override
 	public void onEnable() {
-		this.getProxy().registerChannel("duoauth:filter");
+		this.proxy = this.getProxy();
+		this.proxy.registerChannel("duoauth:filter");
 		this.registerEvents();
 	}
 
 	private void registerEvents() {
-		this.getProxy().getPluginManager().registerListener(this, new Event(this));
+		this.proxy.getPluginManager().registerListener(this, new Event(this));
 	}
 
 }
